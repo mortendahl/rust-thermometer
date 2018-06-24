de35e3226bd5c2c6bc22ef3f33fa2f806c737c05
Robert Vojta <rvojta@me.com>
2018-06-24 21:54:39 +0200

    Add simulate-temperature feature

--- src/temperature.rs
+++ src/temperature.rs
@@ -18,6 +18,7 @@ pub enum Location {
 /// * `location` - thermometer location
 /// * `thermometer` - thermometer device
 /// * `interval` - interval in which we try to read temperature
+#[cfg(not(feature = "simulate-temperature"))]
 pub fn spawn_temperature_reader(
     shared_state: SharedState,
     location: Location,
@@ -37,5 +38,50 @@ pub fn spawn_temperature_reader(
             }
         }
         thread::sleep(interval);
+    });
+}
+
+/// Spawn thread with simulated temperature reader.
+///
+/// # Arguments
+///
+/// * `shared_state` - shared thermometer state
+/// * `location` - thermometer location
+/// * `thermometer` - thermometer device
+/// * `interval` - interval in which we try to read temperature
+#[cfg(feature = "simulate-temperature")]
+pub fn spawn_temperature_reader(
+    shared_state: SharedState,
+    location: Location,
+    _thermometer: Box<Thermometer>,
+    interval: Duration,
+) {
+    use w1::thermometer::Temperature;
+
+    let min = -10_000;
+    let max = 10_000;
+
+    thread::spawn(move || loop {
+        let (mut temp, mut step) = match location {
+            Location::Inside => (min, 1_000),
+            Location::Outside => (max, -500),
+        };
+
+        loop {
+            temp += step;
+
+            if temp == min || temp == max {
+                step *= -1;
+            }
+
+            let t = Temperature::new(temp);
+
+            match location {
+                Location::Inside => shared_state.set_inside_temperature(t),
+                Location::Outside => shared_state.set_outside_temperature(t),
+            }
+
+            thread::sleep(interval);
+        }
     });
 }
