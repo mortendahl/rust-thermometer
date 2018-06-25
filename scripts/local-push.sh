176d6aa4f20f5ee71151794df91c91fc039f9a92
Robert Vojta <rvojta@me.com>
2018-06-25 07:41:37 +0200

    Use local.env for local-push script

--- scripts/local-push.sh
+++ scripts/local-push.sh
@@ -2,19 +2,24 @@
 
 set -e
 
-RESIN_MACHINE_NAME="raspberrypi3"
-DEVICE="cae15d3.local"
+source local.env
 
 DOCKERFILE_TEMPLATE="Dockerfile.template"
 DOCKERFILE="Dockerfile"
 
 echo "Resin machine name: ${RESIN_MACHINE_NAME}"
-echo "Device: ${DEVICE}"
 
 if [ ! -f ${DOCKERFILE_TEMPLATE} ]; then
     echo "Unable to locate ${DOCKERFILE_TEMPLATE} file"
     exit 1
 fi
 
+# We do not want to push target and --ignore doesn't work
+cargo clean
+
 sed "s/%%RESIN_MACHINE_NAME%%/${RESIN_MACHINE_NAME}/g" "${DOCKERFILE_TEMPLATE}" > "${DOCKERFILE}"
-sudo resin local push ${DEVICE} -s . --force-build
+sudo resin local push -s . \
+    --force-build \
+    --app-name local-rust-thermometer \
+    -b "cargo build --target=${RESIN_MACHINE_TARGET} && cp target/${RESIN_MACHINE_TARGET}/debug/thermometer target/debug/thermometer && rm -r target/${RESIN_MACHINE_TARGET}/" \
+    "$@"
