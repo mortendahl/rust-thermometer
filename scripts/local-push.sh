289bacc6afb46bc583d03cfd3e2cd04b771d161e
Robert Vojta <rvojta@me.com>
2018-07-02 15:00:31 +0200

    Add —ignore target

--- scripts/local-push.sh
+++ scripts/local-push.sh
@@ -14,10 +14,13 @@ if [ ! -f ${DOCKERFILE_TEMPLATE} ]; then
     exit 1
 fi
 
-# We do not want to push target and --ignore doesn't work
+# We do not want to push target and --ignore doesn't work, .dockerignore is ignored
+#
+# TODO Move target to `mktemp -d` and then put it back
 cargo clean
 
 sed "s/%%RESIN_MACHINE_NAME%%/${RESIN_MACHINE_NAME}/g" "${DOCKERFILE_TEMPLATE}" > "${DOCKERFILE}"
 sudo resin local push -s . \
     --force-build \
+    --ignore target \
     "$@"
